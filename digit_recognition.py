e52e59450f8233902d85e851dba2125256f05185
SandroMiudo <sandromiudo0@gmail.com>
2024-09-17 23:49:16 +0200

    fixed logic

--- digit_recognition.py
+++ digit_recognition.py
@@ -26,11 +26,9 @@ def sigmoid(x):
 def d_sigmoid(x):
     return sigmoid(x) * (1 - sigmoid(x))
 
-
 def softmax(Z):
     e_x = np.exp(Z - np.max(Z))
     return e_x / e_x.sum(axis=0)
-
 
 def forward_propagation(X,W_0,b_0,W_1,b_1):
     Z_0 = (W_0.dot(X.T)) + b_0 # l x n
@@ -38,8 +36,6 @@ def forward_propagation(X,W_0,b_0,W_1,b_1):
     Z_1 = (W_1.dot(A_0)) + b_1 # t x n 
     A_1 = softmax(Z_1)
     return Z_0,A_0,Z_1,A_1
-
-
 
 def back_propagation(X,Y,W_1,Z_0,A_0,Z_1,A_1):
     n = len(X)
@@ -50,7 +46,6 @@ def back_propagation(X,Y,W_1,Z_0,A_0,Z_1,A_1):
     dW_0 = 1/n * (dZ_0.dot(X)) 
     db_0 = 1/n * np.sum(dZ_0)
     return dW_0,db_0,dW_1,db_1
-
 
 def update_params(dW_1,db_1,dW_0,db_0,W_0,W_1,b_0,b_1,rate):
     W_0 = W_0 - rate * dW_0
@@ -65,28 +60,45 @@ def convert(Y,labels_total):
     ones_y[np.arange(Y.size),Y] = 1
     return ones_y.T
 
-# reminder : setting sample_update too low, might result in bad acc
-def gradient_descent(X,Y,W_0,b_0,W_1,b_1,update='batch',sample_updates=10000,runs=1000,learning_rate=0.1):
-    assert(sample_updates > 0 and runs >= 10 and learning_rate > 0)
-    ones_y = convert(Y,10)
-    arange_X = np.arange(0,len(X))
+def train(X,Y,ValX,ValY,W_0,b_0,W_1,b_1,batch_size=32,epochs=100,learning_rate=0.001):
+    ones_y     = convert(Y,10)
+    ones_val_y = convert(ValY,10)
     dW_0 = 0
     dW_1 = 0
     db_0 = 0
     db_1 = 0
-    for i in range(0,runs+1):
-        Z_0,A_0,Z_1,A_1 = forward_propagation(X,W_0,b_0,W_1,b_1)
-        if(update == "batch-stochastic"):
-            np.random.shuffle(arange_X)
-            dW_0,db_0,dW_1,db_1 = back_propagation(X[arange_X[0:sample_updates],:],ones_y[:,arange_X[0:sample_updates]],W_1,Z_0[:,arange_X[0:sample_updates]],
-                             A_0[:,arange_X[0:sample_updates]],Z_1[:,arange_X[0:sample_updates]],A_1[:,arange_X[0:sample_updates]])
-            W_0,W_1,b_0,b_1 = update_params(dW_1,db_1,dW_0,db_0,W_0,W_1,b_0,b_1,learning_rate) 
-        else:
-            dW_0,db_0,dW_1,db_1 = back_propagation(X,ones_y,W_1,Z_0,A_0,Z_1,A_1)
-        
-        W_0,W_1,b_0,b_1 = update_params(dW_1,db_1,dW_0,db_0,W_0,W_1,b_0,b_1,learning_rate)  
-        if(i % 10 == 0):
-            print("acc on run {} = {}%".format(i,acc(W_0,W_1,b_0,b_1,X,ones_y)))
+
+    batches = len(X) // batch_size
+    remain  = len(X)  % batch_size
+
+    for i in range(0,epochs+1):
+        for j in range(0, batches):
+            _cur_batch_l = j * batch_size
+            _cur_batch_u = _cur_batch_l + batch_size
+            Z_0,A_0,Z_1,A_1 = forward_propagation(X[_cur_batch_l:_cur_batch_u,:],W_0,b_0,W_1,b_1)
+            dW_0,db_0,dW_1,db_1 = back_propagation(X[_cur_batch_l:_cur_batch_u,:],
+                            ones_y[:,_cur_batch_l:_cur_batch_u],
+                            W_1,
+                            Z_0,
+                            A_0,
+                            Z_1,
+                            A_1)
+            W_0,W_1,b_0,b_1 = update_params(dW_1,db_1,dW_0,db_0,W_0,W_1,b_0,b_1,learning_rate)
+        if remain > 0:
+            _cur_batch_l = batches * batch_size
+            _cur_batch_u = _cur_batch_l + remain
+            Z_0,A_0,Z_1,A_1 = forward_propagation(X[_cur_batch_l:_cur_batch_u, :],W_0,b_0,W_1,b_1)
+            dW_0,db_0,dW_1,db_1 = back_propagation(X[_cur_batch_l:_cur_batch_u,:],
+                            ones_y[:,_cur_batch_l:_cur_batch_u],
+                            W_1,
+                            Z_0,
+                            A_0,
+                            Z_1,
+                            A_1)
+            W_0,W_1,b_0,b_1 = update_params(dW_1,db_1,dW_0,db_0,W_0,W_1,b_0,b_1,learning_rate)
+
+        print("train acc on epoch {} = {}%".format(i,acc(W_0,W_1,b_0,b_1,X,ones_y)))
+        print("val acc on epoch {} = {}%".format(i,acc(W_0,W_1,b_0,b_1,ValX,ones_val_y)))
     return W_0,W_1,b_0,b_1
 
 def acc(W_0,W_1,b_0,b_1,X,Y):
@@ -116,29 +128,23 @@ def create_split_set(X,Y,trainSize,random=False):
 
 size = int(0.8 * len(x))
 
-trainX,trainY,testX,testY,devX,devY = create_split_set(x.to_numpy(),np.asarray(y.to_numpy(),dtype=int),size)
-
-print(trainX)
-print(trainY)
+trainX,trainY,testX,testY,devX,devY = create_split_set(x.to_numpy(), np.asarray(y.to_numpy(), dtype=int), size)
 
 W_0,b_0,W_1,b_1 = init_data(10,10,784)
 
-W_0,W_1,b_0,b_1 = gradient_descent(trainX,trainY,W_0,b_0,W_1,b_1,update='batch-stochastic') # output layer
+W_0,W_1,b_0,b_1 = train(trainX,trainY,devX,devY,W_0,b_0,W_1,b_1) # output layer
 
 print("\n\n")
 print("check correctness on dev data:\n")
 
-Z_0,A_0,Z_1,A_1 = forward_propagation(devX,W_0,b_0,W_1,b_1)
+Z_0,A_0,Z_1,A_1 = forward_propagation(testX,W_0,b_0,W_1,b_1)
 errors = 0
-for i in range(0,len(devY)):
+for i in range(0,len(testY)):
     res = np.where(A_1[:,i] == np.max(A_1[:,i]))[0][0]
-    actual_label_i = devY[i]
+    actual_label_i = testY[i]
     if actual_label_i != res:
         errors += 1
     print("sample {} : actual_label = {} --- predicted_label = {}\n".format(i,actual_label_i,res))
 
-acc_dev = round((1 - (errors / devY.size)) * 100,2)
-print("overall acc on dev training = {}%".format(acc_dev))    
-
-
-
+acc_test = round((1 - (errors / testY.size))*100, 2)
+print("overall acc on test data = {}%".format(acc_test))
